cdda09a9b8b78cc260d9a22ade1ddf51e706e50f
Oliphan <bormail68@gmail.com>
2020-03-06 00:19:38 +0000

    Shortened extension display name, updated GUIDs

--- Guids.cs
+++ Guids.cs
@@ -6,8 +6,8 @@ namespace CppJavadoc
 {
     static class GuidList
     {
-        public const string guidCppJavadocPkgString = "70dbb5d8-42d5-43f8-af7b-37668d5c9b46";
-        public const string guidCppJavadocCmdSetString = "7dda296b-3ba8-404a-a15b-bc88d13884b9";
+        public const string guidCppJavadocPkgString = "758530A3-2B09-4C72-9038-891382B3B432";
+        public const string guidCppJavadocCmdSetString = "886634CF-D43A-400F-B932-37C06CDDEEE1";
 
         public static readonly Guid guidCppJavadocCmdSet = new Guid(guidCppJavadocCmdSetString);
     };
